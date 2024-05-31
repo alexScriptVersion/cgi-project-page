3cb31d289546caf8c128ff674b400468a09d0c6f
Alexander Yarar Sellgren <alexyara@nrm.se>
2024-05-31 11:43:09 +0200

    worked on dataProcessing.js

---
+++ js/dataProcessing.js
@@ -0,0 +1,36 @@
+// Include the fs module
+const fs = require("fs");
+const path = require("path");
+
+// Specify the path of the CSV file
+const filePath = "task_statuses.csv";
+
+let outputData;
+
+// Read the CSV file
+fs.readFile(path.resolve(__dirname, filePath), "utf8", (err, data) => {
+  if (err) {
+    console.error("Error while reading:", err);
+    return;
+  }
+
+  // Split the data into lines
+  const lines = data.split("\n");
+
+  // Initialize the output array
+  const output = [];
+
+  // Loop through each line and split it into fields
+  lines.forEach((line) => {
+    const fields = line.split(",");
+    output.push(fields);
+  });
+
+  // Log the output array
+  console.log(output[1]);
+
+  outputData = output;
+
+})
+
+
